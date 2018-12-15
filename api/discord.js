5b362a1e78cf6bfb55890096987f610c75354fa1
broomlytinum <velvetgems@gmail.com>
2018-12-15 13:52:58 -0500

    hes a REALLY bad

--- api/discord.js
+++ api/discord.js
@@ -7,15 +7,17 @@ const fetch = require("node-fetch");
 const btoa = require("btoa");
 const { catchAsync } = require("../client/build/script/utils.js");
 
+const redirect = encodeURIComponent("http://localhost:8080/api/discord/callback");
+
 router.get("/login", (req, res) => {
-  res.redirect(`https://discordapp.com/oauth2/authorize?client_id=${CLIENT_ID}&scope=identify&response_type=code&redirect_uri=${CALLBACK_URL}`);
+  res.redirect(`https://discordapp.com/oauth2/authorize?client_id=${CLIENT_ID}&scope=identify&response_type=code&redirect_uri=${redirect}`);
 });
 
 router.get("/callback", catchAsync(async (req, res) => {
   if (!req.query.code) throw new Error("NoCodeProvided");
   const code = req.query.code;
   const creds = btoa(`${CLIENT_ID}:${CLIENT_SECRET}`);
-  const response = await fetch(`https://discordapp.com/api/oauth2/token?grant_type=authorization_code&code=${code}&redirect_uri=${CALLBACK_URL}`,
+  const response = await fetch(`https://discordapp.com/api/oauth2/token?grant_type=authorization_code&code=${code}&redirect_uri=${redirect}`,
     {
       method: "POST",
       headers: {
