2453edaa504431ec3427890037d099f451567f1b
broomlytinum <velvetgems@gmail.com>
2018-12-15 13:22:04 -0500

    Fixed undefined

--- bot.js
+++ bot.js
@@ -27,7 +27,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-passport.use(new DiscordStrategy({
+app.use(new DiscordStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     callbackURL: process.env.CALLBACK_URL
