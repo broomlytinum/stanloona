05af4261af1c72725ecd2acb2e250779d0b22b53
broomlytinum <velvetgems@gmail.com>
2018-12-15 11:54:22 -0500

    Directory changes

--- bot.js
+++ bot.js
@@ -22,7 +22,7 @@ if (process.env.NODE_ENV === "production") {
 
   const path = require("path");
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, "home.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "home.html"));
   });
 }
 
