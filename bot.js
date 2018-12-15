8dfe28b872beb6f1ea34191986b4d8c273370503
broomlytinum <velvetgems@gmail.com>
2018-12-15 15:35:56 -0500

    Really...ba

--- bot.js
+++ bot.js
@@ -7,31 +7,40 @@ const stream = require("./client/build/script/stream.js");
 app.use("/api/discord", require("./api/discord"));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+	app.use(express.static("client/build"));
 
-  const path = require("path");
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "home.html"));
-  });
-}
+	const path = require("path");
+	app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, "client", "build", "home.html"));
+	});
 
-var url = window.location.href;
-var token = url.searchParams.get("token");
+	app.get('/', (req, res) => {
+	res.send("id: " + req.query.id);
+	});
 
-if (!token) {
+	app.get("/", url_handler);
 
-	app.get("/api/discord/user", user_handler);
-
-	var user_handler = function(req, res){
-		var user = res;
+	var url_handler = function(req, res){
+		var url = res;
 	}
 
-  	if (!user.id) {
+	var token = url.searchParams.get("token");
 
-  		alert(user.id);
-  		stream.load_player();
+	if (!token) {
+
+		app.get("/api/discord/user", user_handler);
+
+		var user_handler = function(req, res){
+			var user = res;
+		}
+
+	  	if (!user.id) {
+
+	  		alert(user.id);
+	  		stream.load_player();
+		}
+
 	}
-
 }
 
 app.listen(process.env.PORT || 8080);
