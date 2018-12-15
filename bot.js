183dec1a52aafdcb9b4e21b2f91d021c036861fd
broomlytinum <39464333+broomlytinum@users.noreply.github.com>
2018-12-15 11:40:26 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,7 +22,7 @@ if (process.env.NODE_ENV === "production") {
 
   const path = require("path");
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "home.html"));
+    res.sendFile(path.resolve("home.html"));
   });
 }
 
