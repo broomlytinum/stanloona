e3d9744b8af54b756ef1ee2d4105e9551c72c977
broomlytinum <velvetgems@gmail.com>
2018-12-15 22:49:45 -0500

    oh my god......

--- api/discord.js
+++ api/discord.js
@@ -51,7 +51,7 @@ router.get("/callback", catchAsync(async (req, res) => {
 
 router.post("/aster", catchAsync(async (req, res) => {
 	console.log(req.body);
-  	sql_client.query(`INSERT INTO users (user_id, amount_aster) VALUES (${req.body.user_id}, ${req.body.amount}) ON CONFLICT (user_id) DO UPDATE SET amount_aster = excluded.amount_aster;`, (err, res) => {
+  	sql_client.query(`INSERT INTO users (user_id, amount_aster) VALUES (${req.body.user_id}, ${req.body.amount});`, (err, res) => {
 		if (err) throw err;
 	});
 }));
