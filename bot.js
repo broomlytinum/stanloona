ff47762d914c46b6ffae1472a91dc4c6430c30fe
broomlytinum <velvetgems@gmail.com>
2018-12-16 12:06:07 -0500

    a commit

--- bot.js
+++ bot.js
@@ -35,9 +35,8 @@ client.on("message", msg => {
   		host: `stan-loona.herokuapp.com`,
 		path: `/api/discord/aster?user_id=${String(msg.author.id)}`
 	}, function (res) {
-		msg.reply(`You have ${JSON.stringify(res.headers)} aster. Thank you for streaming!`);
-	},);
-
+		msg.reply(`You have ${JSON.stringify(res.body)} aster. Thank you for streaming!`);
+	});
   }
 });
 
