1d8398cdebfe3ac64465df6ec53b2808e78cbe14
broomlytinum <velvetgems@gmail.com>
2018-12-16 12:47:30 -0500

    a commit

--- bot.js
+++ bot.js
@@ -32,7 +32,8 @@ client.on("message", msg => {
 
   if (msg.content === "loona.all") {
 
-  	var server_id = client.channels[msg.channelID].guild_id;
+  	console.log(msg.channel_id);
+  	var server_id = client.channels[msg.channel_id].guild_id;
   	console.log(server_id);
 
   }
