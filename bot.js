354ed40baef47c9e5be17fbc5da82f7e4ec93847
broomlytinum <velvetgems@gmail.com>
2018-12-16 12:18:30 -0500

    a commit

--- bot.js
+++ bot.js
@@ -36,11 +36,7 @@ client.on("message", msg => {
 		path: `/api/discord/aster?user_id=${String(msg.author.id)}`
 	}, function (res) {
 		res.on("data", function(chunk) {
-			if (chunk.amount_aster) {
-	    		msg.reply(`You have ${chunk.amount_aster} aster. Thank you for streaming!`);
-	    	} else {
-	    		msg.reply(`You have 0 aster. Visit https://stan-loona.herokuapp.com/ and watch the video completely to earn some.`);
-	    	}
+	    	msg.reply(`You have ${chunk.amount_aster} aster. Thank you for streaming!`);
 	  	});
 	});
   }
