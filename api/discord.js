3d40c30056580704bb34a81376cb051d55f0cada
broomlytinum <velvetgems@gmail.com>
2018-12-15 23:14:33 -0500

    oh my god......

--- api/discord.js
+++ api/discord.js
@@ -58,8 +58,14 @@ router.post("/aster", catchAsync(async (req, res) => {
 
 router.get("/aster", catchAsync(async (req, res) => {
 	console.log(req.body);
-  	sql_client.query(`SELECT ${req.body.user_id} FROM users ORDER BY amount_aster;`, (err, res) => {
+  	sql_client.query(`SELECT * FROM users WHERE (user_id->${req.body.user_id}) IS NOT NULL;`, (err, res) => {
   		console.log(res);
+  		if (res.rows) {
+			for (let row of res.rows) {
+				console.log(row);
+		    	console.log(JSON.stringify(row));
+		  	}
+		}
 	});
 }));
 
